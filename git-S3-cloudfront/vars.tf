df5d116f8615bc72e3f548c95bf15e6d95789eda
Vedant-Mhatre <vedantmh@gmail.com>
2020-10-06 00:59:07 +0530

    minor changes

--- git-S3-cloudfront/vars.tf
+++ git-S3-cloudfront/vars.tf
@@ -2,10 +2,10 @@ variable "region" {
   default = "ap-south-1"
 }
 
-variable "s3_bucket_name"{
+variable "s3_bucket_name" {
   default = "my-tf-test-bucketasdasdasd"
 }
 
-variable "s3_origin_id"{
+variable "s3_origin_id" {
   default = "my-tf-test-bucketasdasdasd.s3.ap-south-1.amazonaws.com"
 }
