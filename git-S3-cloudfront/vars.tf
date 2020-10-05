2d512101b3c0f4a7e8af1d8b8663d234462c84c5
Vedant-Mhatre <vedantmh@gmail.com>
2020-10-06 02:18:34 +0530

    add amis

--- git-S3-cloudfront/vars.tf
+++ git-S3-cloudfront/vars.tf
@@ -7,5 +7,14 @@ variable "s3_bucket_name" {
 }
 
 variable "s3_origin_id" {
-  default = "my-tf-test-bucketasdasdasd.s3.ap-south-1.amazonaws.com"
+  # default = "my-tf-test-bucketasdasdasd.s3.ap-south-1.amazonaws.com"
+  default = "myS3Origin"
+}
+
+variable "amis" {
+  type       = map(string)
+  default = {
+  ap-south-1 = "ami-0cda377a1b884a1bc"
+  us-west-2  = " ami-0947d2ba12ee1ff75"
+  }
 }
