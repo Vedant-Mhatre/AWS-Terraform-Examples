bd4bd820a92dbaf52309a1f40ac31d367734d600
Vedant-Mhatre <vedantmh@gmail.com>
2020-10-06 00:58:54 +0530

    create vpc and security group

---
+++ git-S3-cloudfront/securitygroup.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "allow-web" {
+  name        = "allow-web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.first-vpc.id
+
+  ingress {
+    description = "HTTP traffic from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH traffic from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ingress {
+  #   description = "HTTPS traffic from VPC"
+  #   from_port   = 443
+  #   to_port     = 443
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-web"
+  }
+}
