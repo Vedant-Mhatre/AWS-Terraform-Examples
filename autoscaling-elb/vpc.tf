b564ab8106bffe23d4c0d959037987b857516619
Vedant-Mhatre <vedantmh@gmail.com>
2020-10-27 13:48:46 +0530

    create 3 subnets in different AZs

--- autoscaling-elb/vpc.tf
+++ autoscaling-elb/vpc.tf
@@ -16,17 +16,6 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id                  = aws_vpc.first-vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-
 resource "aws_route_table" "r" {
   vpc_id = aws_vpc.first-vpc.id
 
@@ -45,50 +34,58 @@ resource "aws_route_table" "r" {
   }
 }
 
+
+# First Subnet - ap-south-1a - 10.0.1.0/24
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id                  = aws_vpc.first-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "first-subnet"
+  }
+}
+
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.subnet-1.id
   route_table_id = aws_route_table.r.id
 }
 
+
+# Second Subnet - ap-south-1b - 10.0.4.0/24
+
 resource "aws_subnet" "subnet-2" {
-  vpc_id            = aws_vpc.first-vpc.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "ap-south-1b"
+  vpc_id                  = aws_vpc.first-vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "second-subnet"
   }
 }
 
-# resource "aws_eip" "eip1" {
-#   vpc = true
-# }
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.subnet-2.id
+  route_table_id = aws_route_table.r.id
+}
 
-# resource "aws_nat_gateway" "gw" {
-#   allocation_id = aws_eip.eip1.id
-#   subnet_id     = aws_subnet.subnet-1.id
-# }
+# Third Subnet - ap-south-1c - 10.0.8.0/24
 
-resource "aws_route_table" "p" {
-  vpc_id = aws_vpc.first-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.gw.id
-  }
-
+resource "aws_subnet" "subnet-3" {
+  vpc_id                  = aws_vpc.first-vpc.id
+  cidr_block              = "10.0.8.0/24"
+  availability_zone       = "ap-south-1c"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "second-route-table"
+    Name = "third-subnet"
   }
 }
 
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.subnet-2.id
-  route_table_id = aws_route_table.p.id
+resource "aws_route_table_association" "c" {
+  subnet_id      = aws_subnet.subnet-3.id
+  route_table_id = aws_route_table.r.id
 }
