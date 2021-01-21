9c190e08d3b506a3a9fd2fb91a8a495aa01aa526
vedant-mhatre <vedantmh@gmail.com>
2021-01-21 11:38:19 +0530

    close port 80 and open 443

--- jenkins-django-nginx/securitygroup.tf
+++ jenkins-django-nginx/securitygroup.tf
@@ -3,13 +3,13 @@ resource "aws_security_group" "allow-web_traffic-1" {
   description = "Allow Web inbound traffic"
   vpc_id      = aws_vpc.first-vpc.id
 
-  ingress {
-    description = "HTTP traffic"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   description = "HTTP traffic"
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   ingress {
     description = "SSH traffic"
@@ -27,13 +27,13 @@ resource "aws_security_group" "allow-web_traffic-1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ingress {
-  #   description = "HTTPS traffic from VPC"
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  ingress {
+    description = "HTTPS traffic from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
