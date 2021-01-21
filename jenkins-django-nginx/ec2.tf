7b556b0131080e1ab886a53ceabbd14dcdd5e7c8
vedant-mhatre <vedantmh@gmail.com>
2021-01-21 19:09:15 +0530

    git clone repo is working

--- jenkins-django-nginx/ec2.tf
+++ jenkins-django-nginx/ec2.tf
@@ -10,17 +10,28 @@ resource "aws_instance" "public-web-ec2" {
                 #!/bin/bash
                 sudo apt update -y
                 sudo apt install git openjdk-11-jre -y
-                wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-                sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+                wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+                sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > \
                 /etc/apt/sources.list.d/jenkins.list'
-                sudo apt update -y
+                sudo apt-get update -y
                 sudo apt install jenkins -y
                 sudo systemctl enable --now jenkins
                 sudo ufw allow 8080
                 sudo sh -c 'echo "jenkins ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers'
                 sudo apt install nginx -y
                 sudo ufw allow 'Nginx HTTP'
-                sudo snap install --classic certbot                
+                sudo snap install --classic certbot
+                sudo apt-get install python3-pip python3-dev libpq-dev libmysqlclient-dev -y
+                sudo -H pip3 install --upgrade pip
+                sudo -H pip3 install virtualenv
+                sudo rm -f /var/www/html/*
+                cd /var/www/html/ && sudo git clone -b ${var.branchname} https://${var.username}:${var.password}@gitlab.com/${var.reponame}
+                cd *
+                virtualenv env
+                source env/bin/activate
+                pip install django gunicorn
+                pip install -r requirements.txt
+                cd "$(dirname "$(find . -type f -name settings.py | head -1)")"'
                 EOF
 
   tags = {
