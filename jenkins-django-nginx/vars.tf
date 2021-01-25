16db8d4c2d2afc58b6117cfd7fbc5007fe23d4b0
vedant-mhatre <vedantmh@gmail.com>
2021-01-25 17:34:52 +0530

    added variables to store git credentials

--- jenkins-django-nginx/vars.tf
+++ jenkins-django-nginx/vars.tf
@@ -6,19 +6,27 @@ variable "amis" {
 }
 
 variable "branchname" {
+  default = "uat"
 }
 
 variable "reponame" {
+  default = "constituency-dashboard/dashboard-middleware.git"
 }
 
 variable "username" {
+  default = "abcd"
+  # your git username
 }
 
 variable "password" {
+  default = "abcd"
+  # your password
 }
 
 variable "projectname" {
+  default = "dashboard-middleware"
 }
 
 variable "appname" {
+  default = "ulb"
 }
