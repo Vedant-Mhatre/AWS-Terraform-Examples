889c27201479ff9a5350d39b2af541cd00a6758e
vedant-mhatre <vedantmh@gmail.com>
2021-02-11 23:54:34 +0530

    added vpc, subnet and sec. group

--- jenkins-django-nginx/vars.tf
+++ jenkins-django-nginx/vars.tf
@@ -1,32 +1,51 @@
 variable "region" {
   default = "ap-south-1"
 }
+
 variable "amis" {
   type = map(string)
 }
 
 variable "branchname" {
+  type    = string
   default = "uat"
 }
 
 variable "reponame" {
+  type    = string
   default = "constituency-dashboard/dashboard-middleware.git"
 }
 
 variable "username" {
-  default = "abcd"
+  type    = string
+  default = "vedant-mhatre"
   # your git username
 }
 
 variable "password" {
-  default = "abcd"
+  type    = string
+  default = "dBQriYZA4g2xis2"
   # your password
 }
 
 variable "projectname" {
+  type    = string
   default = "dashboard-middleware"
 }
 
 variable "appname" {
+  type    = string
   default = "ulb"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "securitygroup_id" {
+  type = string
 }
