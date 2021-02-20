2f4919666eff4db48211b297e991e7e601fd728e
vedant-mhatre <vedantmh@gmail.com>
2021-02-20 13:05:19 +0530

    comment everything

--- jenkins-django-nginx/securitygroup.tf
+++ jenkins-django-nginx/securitygroup.tf
@@ -1,48 +1,48 @@
-resource "aws_security_group" "allow-web_traffic-1" {
-  name        = "allow-web_traffic-1"
-  description = "Allow Web inbound traffic"
-  vpc_id      = aws_vpc.first-vpc.id
+# resource "aws_security_group" "allow-web_traffic-1" {
+#   name        = "allow-web_traffic-1"
+#   description = "Allow Web inbound traffic"
+#   vpc_id      = aws_vpc.first-vpc.id
 
-  ingress {
-    description = "HTTP traffic"
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "HTTP traffic"
+#     from_port   = 8000
+#     to_port     = 8000
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    description = "SSH traffic"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "SSH traffic"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    description = "Jenkins traffic"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "Jenkins traffic"
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    description = "HTTPS traffic from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "HTTPS traffic from VPC"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "allow-web_traffic-1"
-  }
-}
+#   tags = {
+#     Name = "allow-web_traffic-1"
+#   }
+# }
--- jenkins-django-nginx/vpc.tf
+++ jenkins-django-nginx/vpc.tf
@@ -1,49 +1,61 @@
-resource "aws_vpc" "first-vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+# resource "aws_vpc" "first-vpc" {
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "dev-vpc"
-  }
-}
+#   tags = {
+#     Name = "dev-vpc"
+#   }
+# }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id            = aws_vpc.first-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+# resource "aws_subnet" "subnet-1" {
+#   vpc_id            = aws_vpc.first-vpc.id
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = "ap-south-1a"
 
-  tags = {
-    Name = "public-subnet"
-  }
-}
+#   tags = {
+#     Name = "public-subnet"
+#   }
+# }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.first-vpc.id
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.first-vpc.id
 
-  tags = {
-    Name = "dev-gw"
-  }
-}
+#   tags = {
+#     Name = "dev-gw"
+#   }
+# }
 
-resource "aws_route_table" "r" {
-  vpc_id = aws_vpc.first-vpc.id
+# resource "aws_route_table" "r" {
+#   vpc_id = aws_vpc.first-vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
 
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.gw.id
-  }
+#   route {
+#     ipv6_cidr_block = "::/0"
+#     gateway_id      = aws_internet_gateway.gw.id
+#   }
 
-  tags = {
-    Name = "dev-route-table"
-  }
-}
+#   tags = {
+#     Name = "dev-route-table"
+#   }
+# }
 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet-1.id
-  route_table_id = aws_route_table.r.id
-}
+# resource "aws_route_table_association" "a" {
+#   subnet_id      = aws_subnet.subnet-1.id
+#   route_table_id = aws_route_table.r.id
+# }
+
+# output "vpcid" {
+#   value = aws_vpc.first-vpc.id
+# }
+
+# output "subid" {
+#   value = aws_subnet.subnet-1.id
+# }
+
+# output "securityid" {
+#   value = aws_security_group.allow-web_traffic-1.id
+# }
